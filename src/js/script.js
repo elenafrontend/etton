ae666fb02f115c723458f1f902741e236b6160ec
elena.g <gumerova.e.p@ya.ru>
2021-06-05 23:49:06 +0300

    scripts - modal box

--- src/js/script.js
+++ src/js/script.js
@@ -33,9 +33,43 @@ burgerBtn.addEventListener("click", function () {
     body.style.paddingRight = "0px";
     header.style.paddingRight = "0px";
   }
-
-	
-
-
-
 });
+
+
+// modal box
+
+let addBtn = document.querySelector('.add-button');
+let modalSection = document.querySelector('.modal');
+let cancelBtn = document.querySelector('.modal__button--cancel');
+
+addBtn.addEventListener('click', function() {
+	modalSection.classList.add('modal--show');
+	document.body.style.overflow = "hidden";
+
+	let body = document.body;
+  let paddingOffset = window.innerWidth - body.offsetWidth + "px";
+	
+	body.style.paddingRight = paddingOffset;
+  header.style.paddingRight = paddingOffset;
+})
+
+cancelBtn.addEventListener('click', function() {
+	modalSection.classList.remove('modal--show');
+
+	document.body.style.overflow = "";
+  body.style.paddingRight = "0px";
+  header.style.paddingRight = "0px";
+})
+
+// Modal checkbox
+
+let checkboxSnow =  document.querySelector('.modal__forms-checkbox');
+let submitBtn = document.querySelector('.modal__button--ok');
+
+checkboxSnow.addEventListener('click', function() {
+	if (checkboxSnow.checked) {
+		submitBtn.disabled = false;
+	} else {
+		submitBtn.disabled = true;
+	}
+})
